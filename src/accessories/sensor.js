2ae09c308e47df292f0085bc35486ffd839ef787
meg768 <meg768@users.noreply.github.com>
2023-10-13 18:36:37 +0200

    -

--- src/accessories/sensor.js
+++ src/accessories/sensor.js
@@ -4,24 +4,27 @@ var Accessory = require('../accessory.js');
 
 module.exports = class extends Accessory {
 
+
+
+
     constructor(options) {
 
 		super(options);
 		
 		if (this.device.capabilitiesObj['alarm_motion']) {
-			this.addService(new Service.MotionSensor(this.name, this.UUID));
+			this.addService(new Service.MotionSensor(this.name + ' Rörelse', this.UUID));
 			this.enableMotionDetected(Service.MotionSensor);	
 		}
 		if (this.device.capabilitiesObj['measure_temperature']) {
-			this.addService(new Service.TemperatureSensor(this.name, this.UUID));
+			this.addService(new Service.TemperatureSensor(this.name + ' Temperatur', this.UUID));
 			this.enableCurrentTemperature(Service.TemperatureSensor);		
 		}
 		if (this.device.capabilitiesObj['measure_luminance']) {
-			this.addService(new Service.LightSensor(this.name, this.UUID));
+			this.addService(new Service.LightSensor(this.name + ' Luminans', this.UUID));
 			this.enableLightSensor(Service.LightSensor);		
 		}
 		if (this.device.capabilitiesObj['measure_humidity']) {
-			this.addService(new Service.HumiditySensor(this.name, this.UUID));
+			this.addService(new Service.HumiditySensor(this.name + ' Luftfuktighet', this.UUID));
 			this.enableLightSensor(Service.HumiditySensor);		
 		}
 
