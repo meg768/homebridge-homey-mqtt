63e9eb895c97c2dcedbefee1ab44aad4ce27152b
meg768 <magnus@egelberg.se>
2024-12-04 21:06:26 +0100

    -

--- src/platform.js
+++ src/platform.js
@@ -30,61 +30,9 @@ module.exports = class Platform {
 
 
 
-	createAccessory(device) {
-
-        let Socket =  require('./accessories/socket.js');
-        let Light = require('./accessories/light.js');
-        let TV = require('./accessories/tv.js');
-        let Sensor = require('./accessories/sensor.js');
-        let Switch = require('./accessories/switch.js');
-        
-        let Accessory = undefined;
-
-        switch (device.class) {
-            case 'tv': {
-                Accessory = TV; 
-                break;
-            }
-            case 'socket': {
-                Accessory = Socket;
-                break;
-            }
-            case 'sensor': {
-                Accessory = Sensor;
-                break;
-            }
-            case 'light': {
-                Accessory = Light;
-                break;
-            }
-            default: {
-                if (device.capabilitiesObj && device.capabilitiesObj.onoff) {
-                    Accessory = Switch;
-                }
-                /*
-                if (device.capabilitiesObj && device.capabilitiesObj.measure_temperature) {
-                    Accessory = Sensor;
-                }
-                if (device.capabilitiesObj && device.capabilitiesObj.measure_humidity) {
-                    Accessory = Sensor;
-                }
-                */
-                
-                break;
-            }
-        }
-
-        
-        if (Accessory != undefined) {
-            return new Accessory({device:device, platform:this});
-        }
-	
-
-	}
-
 	createAccessories(devices) {
 
-        let Socket =  require('./accessories/socket.js');
+        let Socket = require('./accessories/socket.js');
         let Light = require('./accessories/light.js');
         let TV = require('./accessories/tv.js');
         let Sensor = require('./accessories/sensor.js');
@@ -99,12 +47,12 @@ module.exports = class Platform {
             let device = devices[key];
 
 			if (this.config.exclude && this.config.exclude.indexOf(device.id) >= 0) {
-				this.debug(`Excluding device ${device.zoneName}/${device.name}.`);
+				this.debug(`Excluding device ${device.name}.`);
 				continue;
 			}
 
             if (!device.capabilitiesObj) {
-                this.debug(`Ignoring device ${device.zoneName}/${device.name}. No capabilities.`);
+                this.debug(`Ignoring device ${device.name}. No capabilities.`);
                 continue;
             }
 
@@ -144,7 +92,7 @@ module.exports = class Platform {
             }
 			
 			if (Accessory != undefined) {
-				this.debug(`Adding device ${device.zoneName}/${device.name}.`);
+				this.debug(`Adding device ${device.name}.`);
                 accessories.push(new Accessory({device:device, platform:this}));
 			}
 	
