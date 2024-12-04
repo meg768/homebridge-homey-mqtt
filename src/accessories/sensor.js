87dace6f010da3fe305514ee222da154399d775d
meg768 <magnus@egelberg.se>
2024-12-04 23:02:31 +0100

    -

--- src/accessories/sensor.js
+++ src/accessories/sensor.js
@@ -13,19 +13,19 @@ module.exports = class extends Accessory {
 		
 
 		if (this.device.capabilitiesObj['alarm_motion']) {
-			this.addService(new Service.MotionSensor(`${this.name}, rörelse`, this.UUID));
+			this.addService(new Service.MotionSensor(`${this.name} - rörelse`, this.UUID));
 			this.enableMotionDetected(Service.MotionSensor);	
 		}
 		if (this.device.capabilitiesObj['measure_temperature']) {
-			this.addService(new Service.TemperatureSensor(`${this.name}, temperatur`, this.UUID));
+			this.addService(new Service.TemperatureSensor(`${this.name} - temperatur`, this.UUID));
 			this.enableCurrentTemperature(Service.TemperatureSensor);		
 		}
 		if (this.device.capabilitiesObj['measure_luminance']) {
-			this.addService(new Service.LightSensor(`${this.name}, ljusstyrka`, this.UUID));
-			this.enableLightSensor(Service.LightSensor);		
+			this.addService(new Service.LightSensor(`${this.name} - ljusstyrka`, this.UUID));
+			this.enableLightSensor(Service.LightSensor);			
 		}
 		if (this.device.capabilitiesObj['measure_humidity']) {
-			this.addService(new Service.HumiditySensor(`${this.name}, luftfuktighet`, this.UUID));
+			this.addService(new Service.HumiditySensor(`${this.name} - luftfuktighet`, this.UUID));
 			this.enableCurrentRelativeHumidity(Service.HumiditySensor);		
 		}
 
