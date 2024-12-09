8674c52413f6e5d2a13b5bf70c703ecd625b67f0
meg768 <magnus@egelberg.se>
2024-12-09 21:13:28 +0100

    -

--- src/capabilities/dim.js
+++ src/capabilities/dim.js
@@ -1,5 +1,5 @@
 let { API, Service, Characteristic } = require('./../homebridge.js');
-let Capability = require('./capability.js');
+let Capability = require('../capability.js');
 
 
 module.exports = class extends Capability {
