1cffed927202b3cc69e6572cd3d17df245c531eb
meg768 <magnus@egelberg.se>
2024-12-20 23:31:54 +0100

    -

---
+++ src/capabilities/sensor-luminance.js
@@ -0,0 +1,10 @@
+let { API, Service, Characteristic } = require('./../homebridge.js');
+let Capability = require('../capability.js');
+
+module.exports = class extends Capability {
+
+	
+    getCharacteristic() {
+        return this.service.getCharacteristic(Characteristic.CurrentAmbientLightLevel);
+    }
+};
