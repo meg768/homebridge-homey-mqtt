8e7985f91bad24a6a0f0626e1cc141ace70c3f53
meg768 <magnus@egelberg.se>
2024-12-22 23:11:51 +0100

    -

--- src/capabilities/button.js
+++ src/capabilities/button.js
@@ -6,13 +6,19 @@ module.exports = class extends Capability {
 
     enableCapability() {
 		let programmableSwitchEvent = this.service.getCharacteristic(Characteristic.ProgrammableSwitchEvent);
+
+        let currentValue = this.getCapabilityValue();
         let capabilityID = this.getCapabilityID();
 
-/*        programmableSwitchEvent.onGet(async () => {
-            return currentValue ? Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS;
+
+        programmableSwitchEvent.onGet(async () => {
+            return Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS;
         });
-*/
+
+
         this.accessory.on(capabilityID, (value) => {
+
+            currentValue = value;
 
             if (value) {
                 this.debug(`Updating ${this.accessory.name}/${capabilityID}:${value}`);
