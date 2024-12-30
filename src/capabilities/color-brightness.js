aee5c58fdd5398db2cecb115426bbd00acbeca89
meg768 <magnus@egelberg.se>
2024-12-30 20:12:59 +0100

    -

--- src/capabilities/color-brightness.js
+++ src/capabilities/color-brightness.js
@@ -3,26 +3,27 @@ let Capability = require('../capability.js');
 
 
 module.exports = class extends Capability {
-
     getCharacteristic() {
         return this.service.getCharacteristic(Characteristic.Brightness);
     }
+    toHomeKit(value) {
+        return value * 100;
 
-    toHomeKit = (value) => {
         let characteristic = this.getCharacteristic();
         let capability = this.getCapability();
 
         value = (value - capability.min) / (capability.max - capability.min);
         value = value * (characteristic.props.maxValue - characteristic.props.minValue) + characteristic.props.minValue;
         return value;
-    };
+    }
 
-    toHomey = (value) => {
+    toHomey(value) {
+        return value / 100;
         let characteristic = this.getCharacteristic();
         let capability = this.getCapability();
 
         value = (value - characteristic.props.minValue) / (characteristic.props.maxValue - characteristic.props.minValue);
         value = value * (capability.max - capability.min) + capability.min;
         return value;
-    };
+    }
 };
