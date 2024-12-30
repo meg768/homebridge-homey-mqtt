8bda7467f6be9b2c13a25738bdc9473798548d9b
meg768 <magnus@egelberg.se>
2024-12-30 19:58:50 +0100

    -

--- src/capabilities/color-luminance.js
+++ src/capabilities/color-luminance.js
@@ -2,7 +2,24 @@ let { API, Service, Characteristic } = require('./../homebridge.js');
 let Capability = require('../capability.js');
 
 module.exports = class extends Capability {
+    
+    toHomeKit(value) {
+        let characteristic = this.getCharacteristic();
+        let capability = this.getCapability();
 
+        value = (value - capability.min) / (capability.max - capability.min);
+        value = value * (characteristic.props.maxValue - characteristic.props.minValue) + characteristic.props.minValue;
+        return value;
+    }
+
+    toHomey(value) {
+        let characteristic = this.getCharacteristic();
+        let capability = this.getCapability();
+
+        value = (value - characteristic.props.minValue) / (characteristic.props.maxValue - characteristic.props.minValue);
+        value = value * (capability.max - capability.min) + capability.min;
+        return value;
+    }
 
     getCharacteristic() {
         return this.service.getCharacteristic(Characteristic.CurrentAmbientLightLevel);
